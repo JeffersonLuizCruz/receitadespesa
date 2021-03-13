8af299750f6148390b117f7894ccb475a9be381b
Jefferson <jefferson.luiz.cruz@gmail.com>
2021-03-12 23:31:03 -0300

    Category - Concluído

--- src/main/java/com/financial/service/CategoryServiceImpl.java
+++ src/main/java/com/financial/service/CategoryServiceImpl.java
@@ -1,28 +1,33 @@
 package com.financial.service;
 
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.financial.entity.Category;
+import com.financial.exceptions.NotFoundException;
+import com.financial.repository.CategoryRepository;
 import com.financial.serviceinterfaces.CategoryServiceInterfaces;
 
 public class CategoryServiceImpl implements CategoryServiceInterfaces{
+	
+	@Autowired private CategoryRepository categoryRepository;
 
 	@Override
 	public Category save(Category category) {
-		// TODO Auto-generated method stub
-		return null;
+		return categoryRepository.save(category);
 	}
 
 	@Override
 	public Category getById(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<Category> result = categoryRepository.findById(id);
+		return result.orElseThrow(() -> new NotFoundException("Não existe categoria com esse id: " + id));
 	}
 
 	@Override
 	public List<Category> listAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return categoryRepository.findAll();
 	}
 	
 	
